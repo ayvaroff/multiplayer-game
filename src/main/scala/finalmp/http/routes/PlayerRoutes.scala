7a5b8da615bb6511e3242daaa800361484c442ce
Anton Ayvarov <ayvaroff@gmail.com>
2022-07-14 15:12:58 +0300

    Add basic Player and PlayerController models

--- src/main/scala/finalmp/http/routes/PlayerRoutes.scala
+++ src/main/scala/finalmp/http/routes/PlayerRoutes.scala
@@ -2,15 +2,21 @@ package finalmp.http.routes
 
 import cats.effect._
 import cats.syntax.all._
+import io.circe.syntax._
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.circe.CirceEntityCodec._
+import finalmp.controllers.PlayerController
 
-class PlayerRoutes[F[_]: Sync] extends Http4sDsl[F] {
+class PlayerRoutes[F[_]: Sync](
+  controller: PlayerController
+) extends Http4sDsl[F] {
   def routes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "hello" / name =>
       Ok(s"Hello, $name!")
 
-    case req @ POST -> Root / "hello" =>
-      Ok(req.as[String].map(name => s"Hello again, $name!"))
+    case req @ POST -> Root / "connect" => {
+      Ok(controller.addPlayer().asJson.noSpaces)
+    }
   }
 }
